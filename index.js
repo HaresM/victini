3e1608f7996171819f54ddbec3c709558773247a
Hares Mahmood <hmahmood229@gmail.com>
2017-10-27 21:55:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,9 +52,30 @@ bot.on("message", (message) => {
     
   if (!message.content.startsWith(prefix)) return;
 
-  if (message.content.startsWith(prefix + "lenny")) {
-        message.delete();
-        message.channel.send("( ͡° ͜ʖ ͡°)");
+  if (message.content.startsWith(prefix + "8ball")) {
+        var magicArray = [
+      'It is certain.',
+	    'It is decidedly so.',
+	    'Without a doubt.',
+	    'Yes - definitely.',
+	    'You may rely on it.',
+	    'As I see it, yes.',
+	    'Most likely.',
+	    'Outlook good.',
+	    'Yes.',
+	    'Signs point to yes.',
+	    'Reply hazy, try again.,
+	    'Ask again later.',
+	    'Better not tell you now.',
+	    'Cannot predict now.',
+	    'Concentrate and ask again.',
+	    'I would not count on it.,
+	    'My reply is no.',
+	    'My sources say no.,
+	    'Outlook not so good.',
+	    'Very doubtful.'
+    ];
+    var randomReply = Math.floor(Math.random()*magicArray.length);
   } else
   if (message.content.startsWith(prefix + "shrug")) {
         message.delete();
