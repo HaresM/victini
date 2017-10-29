b3934b943f621b8b77a6a7d5ea30156881190522
Hares Mahmood <hmahmood229@gmail.com>
2017-10-29 18:53:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const prefix = "v.";
 
 client.on('guildMemberAdd', member => {
 
-    bot.channels.get('369507173937709056').send('Welcome to the official Pokémon Victorius server, ' + member.user + ' ! To proceed, please type in a separate message the number which corresponds the most to the reason you have come to this server. \n\n1)    I want to support the game but do not wish to contribute anything. (Type in `1`) \n2)   I want to help the game by contributing something, but do not want to be extremely commited. (Type in `2`) \n3)   I want to actively help the game and its development by providing aid in one particular field of which I am skilled at. (Type in `3`)\n\nFeel free to ask the <@&369499519794151425>, <@&369499281134059520>, or an <@&372096917611741184> for help!');
+    client.channels.get('369507173937709056').send('Welcome to the official Pokémon Victorius server, ' + member.user + ' ! To proceed, please type in a separate message the number which corresponds the most to the reason you have come to this server. \n\n1)    I want to support the game but do not wish to contribute anything. (Type in `1`) \n2)   I want to help the game by contributing something, but do not want to be extremely commited. (Type in `2`) \n3)   I want to actively help the game and its development by providing aid in one particular field of which I am skilled at. (Type in `3`)\n\nFeel free to ask the <@&369499519794151425>, <@&369499281134059520>, or an <@&372096917611741184> for help!');
 
     var roleIntro = member.guild.roles.find('name', 'Intro');
     member.addRole(roleIntro);
@@ -15,7 +15,7 @@ client.on('guildMemberAdd', member => {
 
   client.on('guildMemberRemove', member => {
 
-    bot.channels.get('369492433592909844').send('Sadly, ' + member.user.username + ' has just left the server. RIP...!');
+    client.channels.get('369492433592909844').send('Sadly, ' + member.user.username + ' has just left the server. RIP...!');
 
   });
 
