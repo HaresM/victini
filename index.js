717d8b1dddd22a80e02be29daa38775d04048fe9
Hares Mahmood <hmahmood229@gmail.com>
2017-11-23 19:12:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ client.on("message", (message) => {
                 message.channel.send("While we haven't confirmed anything, we are aiming to release a demo around February\March 2018!");
             } else 
             if (args[0] === "story") {
-                message.channel.send("```Welcome to your new adventure! Travel across the beautiful landscape of Scaniola with your Pokemon, uncovering secrets about the region and what lies above. Scaniola is home to more than just tourist hotspots; it has a vast history intertwined with ancient cities and new pokemon. Rivalry and friendship will follow on this journey, as you travel alongside two trainers-in-the-making, Zeak and Lisa. Conquer gyms and challenge the Elite Four, before facing the Champion of Scaniola. Professor Aspen recruits your help when strange anomalies threaten the natural order of things! And don’t forget to stop the bad guys!```"
+                message.channel.send("```Welcome to your new adventure! Travel across the beautiful landscape of Scaniola with your Pokemon, uncovering secrets about the region and what lies above. Scaniola is home to more than just tourist hotspots; it has a vast history intertwined with ancient cities and new pokemon. Rivalry and friendship will follow on this journey, as you travel alongside two trainers-in-the-making, Zeak and Lisa. Conquer gyms and challenge the Elite Four, before facing the Champion of Scaniola. Professor Aspen recruits your help when strange anomalies threaten the natural order of things! And don’t forget to stop the bad guys!```")
             }
         }
     }
