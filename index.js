d00f7fccb96d0974c9c671d632a993886671e2c2
Hares Mahmood <hmahmood229@gmail.com>
2017-11-28 18:54:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,11 @@ client.on('guildMemberAdd', member => {
 		client.channels.get('369507173937709056').send('Welcome to the official Pokémon Victorius server, ' + member.user + ' ! To proceed, please type in a separate message the number which corresponds the most to the reason you have come to this server. \n\n1)    I want to support the game but do not wish to contribute anything. (Type in `1`) \n2)   I want to help the game by contributing something, but do not want to be extremely commited. (Type in `2`) \n3)   I want to actively help the game and its development by providing aid in one particular field of which I am skilled at. (Type in `3`)\n\nFeel free to ask the <@&369499519794151425>, <@&369499281134059520>, or an <@&372096917611741184> for help!');
 		var roleIntro = member.guild.roles.find('name', 'Intro');
 		member.addRole(roleIntro);
-	} else {
+	} 
+	else if (member.guild.id === "383287520118702100") {
+		member.send("Welcome to the official Pokémon Equality server! You have to wait 10 minutes to post something in the server, but please read the rules and FAQ-channels. Enjoy your stay!");
+	}
+	else {
 		defaultChannel.send(member + ' has joined the server. Welcome!');
 	}
 });
