7882cef1a229220b94ec1ef9a1528c2d950d001a
Nathaniel Chin <thegamingorangutans@gmail.com>
2017-12-01 13:31:33 +0800

    Update botbroken.js

--- botbroken.js
+++ botbroken.js
@@ -2,7 +2,10 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on("message", (message) => {
   if (message.content.startsWith("v.")) {
-    message.channel.send("Victini is currently broken use furret for the time being");
+    message.channel.send("Victini is currently broken use furret for the time being.\nFor furret's invite link do v.furret");
+  }
+  if (message.content.startsWith("v.furret")) {
+    message.channel.send("Invite Link To Furret:\nhttps://discordapp.com/oauth2/authorize?&client_id=370889343843434497&scope=bot&permissions=8");
   }
 });
 client.on("ready", () => {
