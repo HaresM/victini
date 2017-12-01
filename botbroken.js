de93d260b62ea42471637369535d48100ebc490a
Nathaniel Chin <thegamingorangutans@gmail.com>
2017-12-01 13:08:30 +0800

    Update botbroken.js

--- botbroken.js
+++ botbroken.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.on('ready' => {
+client.on('ready', () => {
   client.user.setGame("Victini is currently broken use furret for the time being");
   client.user.setStatus("dnd");
 })
