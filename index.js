cc631bd63bf7e9b209b6f88943609028dbe39a21
Hares Mahmood <hmahmood229@gmail.com>
2017-12-01 14:11:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -34,9 +34,8 @@ function botExec(member) {
 	return hasRole(member, "Victini Exec") || member.user.id == member.guild.ownerID || member.user.id == '311534497403371521';
 }
 
-if (member.guild.id === "383287520118702100") {
-	member.send("Welcome to the official Pokémon Equality server! You have to wait 10 minutes to post something in the server, but please read the rules and FAQ-channels. Enjoy your stay!");
-}
+
+
 client.on('guildMemberAdd', member => {
 	if (member.guild.id === "383287520118702100") {
 		member.send("Welcome to the official Pokémon Equality server! You have to wait 10 minutes to post something in the server, but please read the rules and FAQ-channels. Enjoy your stay!");
