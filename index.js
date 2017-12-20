866b958381c31826be4a20f74729e25c3fa61420
Hares Mahmood <hmahmood229@gmail.com>
2017-12-20 20:24:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -126,15 +126,6 @@ client.on("message", message => {
     }
 
 
-    if (message.author.bot) return;
-    if (message.content.indexOf(config.prefix) !== 0) return;
-
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-
-
-
     if (command === "lenny") {
         message.delete();
         message.channel.send("( ͡° ͜ʖ ͡°)");
