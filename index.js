034154f2f05b4a25b4668bf5397aa1b456be1e4d
Hares Mahmood <hmahmood229@gmail.com>
2017-12-20 20:17:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ client.on("guildMemberRemove", member => {
 
 
 
-client.on("message", (message) => {
+client.on("message", message => {
 
     if (message.author.bot) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
@@ -259,7 +259,7 @@ client.on("message", (message) => {
 
         }
 
-    )};
+    });
 
 
 
