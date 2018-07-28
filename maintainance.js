061897d7778b8ee9400771de90a7688fa5991590
Glitch (vicitnibot) <none>
2018-07-28 05:18:58 +0000

    🍿💓 Updated with Glitch

--- maintainance.js
+++ maintainance.js
@@ -4,4 +4,9 @@ client.on("ready", () => {
   client.user.setActivity("Victini is down for maintainance.");
   client.user.setStatus("dnd");
 });
+client.on("message", (message) => {
+  if (message.content.startsWith("v.restart")) {
+    process.exit(1);
+  }
+});
 client.login(process.env.TOKEN);
